1aac3e7d220d0b1e502ca7f07535c64b4707e79f
Samrita Bakshi <samrita.bakshi@neogrowth.in>
2019-04-25 10:19:24 +0530

    value

--- lib/casino/ldap_authenticator.rb
+++ lib/casino/ldap_authenticator.rb
@@ -84,11 +84,11 @@ class CASino::LDAPAuthenticator
         if value
           if index_ldap == "objectguid"
             p "111"
-            p value.to_uri
+            p value[0].to_uri
             p "222"
-            p value.hexdigest
+            p value[0].hexdigest
             p "333"
-            p value.inspect
+            p value[0].inspect
             p "0000"
             uuid = UUIDTools::UUID.parse_raw(value.to_s)
             p "uuid"
